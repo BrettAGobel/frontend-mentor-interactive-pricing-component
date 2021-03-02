019886443739212e92bb3cb92696230c7edd3951
BrettAGobel <brettg841@gmail.com>
2021-03-02 11:53:43 -0700

    kinda got the pricing slider working, few more kinks to work out with active up-dating after billing toggle is switched

--- interactive-pricing-component-main/script.js
+++ interactive-pricing-component-main/script.js
@@ -16,17 +16,39 @@ window.onload = function () {
 
 
 function showResult() {
+    let yBilling = document.getElementById('switch')
     const pageViews = document.getElementById('total-views')
+    let newPrice = document.getElementById('new-price')
     pageViews.style.color = 'yellow'
     const viewScale = document.getElementById('views')
-    function setViewsEventListener () {
 
+//changes pageviews number to whatever the range slider chages to
+    function setViewsEventListener () {
+            let newViewValue = viewScale.value
             viewScale.addEventListener('change', () => {
-                console.log('page views: ', viewScale.value)
+                // console.log('page views: ', viewScale.value)
+                let price = Math.round((viewScale.value * .0005))
+                if (yBilling.checked === true) {
+                    price = price * 0.75
+                }
+
                 pageViews.innerText = "Page Views: " + viewScale.value
+                newPrice.innerText = `$${price}/ month`
+
 
         })
+
+        console.log(newViewValue)
     }
+
+    // function updatePrice () {
+    //
+    //     viewScale.addEventListener('change', () => {
+    //
+    //
+    //     })
+    // }
+
     setViewsEventListener()
 
 }
