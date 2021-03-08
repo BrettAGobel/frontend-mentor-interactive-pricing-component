a02eb891ae9b5597310745cd37be180cfa60b365
BrettAGobel <brettg841@gmail.com>
2021-03-08 12:11:56 -0700

    took a while but got the event listeners to work appropriatly

--- interactive-pricing-component-main/script.js
+++ interactive-pricing-component-main/script.js
@@ -1,60 +1,50 @@
-// const toggle = document.querySelector('[aria-pressed]');
-//
-// toggle.addEventListener('click', (e) => {
-//     let pressed = e.target.getAttribute('aria-pressed') === 'true';
-//     e.target.setAttribute('aria-pressed', String(!pressed));
-// });
 window.onload = function () {
 
     showResult()
-    pageViews.innerText = "Page Views: " + viewScale.value
+
 };
-
-// function amIOn() {
-//     alert('is this thing on?')
-// };
-
-
 
 
 function showResult() {
     let yBilling = document.getElementById('switch')
     const pageViews = document.getElementById('total-views')
     let newPrice = document.getElementById('new-price')
-    pageViews.style.color = 'yellow'
+
     const viewScale = document.getElementById('views')
 
-    let newViewValue = viewScale.value
 
-//changes pageviews number to whatever the range slider chages to
+
+//changes pageviews number to whatever the range slider changes to.  Only updates after change event has finished
     function setViewsEventListener () {
 
-            viewScale.addEventListener('change', (e) => {
-                // console.log('page views: ', viewScale.value)
-                let price = Math.round((viewScale.value * .0005))
-                if (yBilling.checked == true) {
-                    price = price * 0.75
-                    newPrice.innerText = `$${price}/ month`
+            viewScale.addEventListener('input', (e) => {
+                let newViewValue = viewScale.value
+                let price = Math.round(newViewValue)
+                if (yBilling.checked) {
+                    pageViews.innerText = "Page Views: " + newViewValue +"k"
+                    newPrice.innerText = `$${viewScale.value * 0.75}/ month`
                 }
-                pageViews.innerText = "Page Views: " + viewScale.value
-                newPrice.innerText = `$${price}/ month`
+                else {
+                    pageViews.innerText = "Page Views: " + newViewValue +"k"
+                    newPrice.innerText = `$${price}/ month`}
+                })
 
 
-        })
-
-        console.log(newViewValue)
     }
 
-    // function updatePrice () {
-    //
-    //     yBilling.addEventListener('change', () => {
-    //             if (yBilling.checked === true) {
-    //                 newPrice.innerText =
-    //             }
-    //
-    //     })
-    // }
+    function yearlyBilling () {
+        yBilling.addEventListener('click', (e) => {
+            if (yBilling.checked === true) {
 
+                newPrice.innerText = `$${viewScale.value * 0.75}/ month`
+            }
+            else {
+                newPrice.innerText = `$${viewScale.value * 1.0}/ month`
+            }
+
+        })
+    }
+    yearlyBilling()
     setViewsEventListener()
 
 }
