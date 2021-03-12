383a0740b963382888caa838bdadcc1aa8e36419
BrettAGobel <brettg841@gmail.com>
2021-03-12 11:26:49 -0700

    progressive color added to slider

--- interactive-pricing-component-main/script.js
+++ interactive-pricing-component-main/script.js
@@ -9,7 +9,6 @@ function showResult() {
     let yBilling = document.getElementById('switch')
     const pageViews = document.getElementById('total-views')
     let newPrice = document.getElementById('new-price')
-
     const viewScale = document.getElementById('views')
 
 
@@ -17,7 +16,7 @@ function showResult() {
 //changes pageviews number to whatever the range slider changes to.  Only updates after change event has finished
     function setViewsEventListener () {
 
-            viewScale.addEventListener('input', (e) => {
+            viewScale.addEventListener('input', () => {
                 let newViewValue = viewScale.value
                 let price = Math.round(newViewValue)
                 if (yBilling.checked) {
@@ -27,13 +26,18 @@ function showResult() {
                 else {
                     pageViews.innerText = "Page Views: " + newViewValue +"k"
                     newPrice.innerText = `$${price}/ month`}
-                })
+
+                const valuePercent =  (viewScale.value/viewScale.max)*100;
+                viewScale.style.background = `linear-gradient(to right, hsl(174, 77%, 80%), hsl(174, 77%, 80%) ${valuePercent}%, hsl(224, 65%, 95%) ${valuePercent}%, hsl(224, 65%, 95%) 100%)`;
+
+
+            })
 
 
     }
 
     function yearlyBilling () {
-        yBilling.addEventListener('click', (e) => {
+        yBilling.addEventListener('click', () => {
             if (yBilling.checked === true) {
 
                 newPrice.innerText = `$${viewScale.value * 0.75}/ month`
@@ -44,6 +48,10 @@ function showResult() {
 
         })
     }
+
+
+
+
     yearlyBilling()
     setViewsEventListener()
 
